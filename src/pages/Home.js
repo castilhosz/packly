30803a288b52524db286e32fddd286ea11e70464
arthrc <arthurcastilhos@gmail.com>
2020-10-09 14:00:21 -0300

    stylized Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,21 +12,22 @@ function Home() {
   }
 
   return(
-    <View>
+    <View style={{ flex:1 }}>
       <View style={styles.containerTitle}>
         <Text style={styles.titleText}>Welcome to</Text>
         <Image style={styles.titleImage} source={require('../assets/logotext.png')}/>
       </View>
-      <View>
-        <Text>Track your package</Text>
+      <View style={styles.containerTrack}>
+        <Text style={styles.trackText}>Track your package</Text>
         <TextInput 
         placeholder="Correios's code"
         value={code}
-        onChangeText={setCode}/>
+        onChangeText={setCode}
+        style={styles.trackInput}/>
         <BaseButton
         onPress={handleNavigateToPackagePage}
-        >
-          <Text>Let's track!</Text>
+        style={styles.trackButtonContainer}>
+          <Text style={styles.trackButtonText}>Let's track it!</Text>
         </BaseButton>
       </View>
     </View>
@@ -49,7 +50,40 @@ const styles = StyleSheet.create({
     width: 400,
     height: 99,
     top: -25
-  }
+  },
+  containerTrack: {
+    flex: 3,
+    position: 'relative',
+    paddingHorizontal: 16,
+  },
+  trackText: {
+    fontFamily: 'Inter_600SemiBold',
+    fontSize: 16,
+    color: '#000'
+  },
+  trackInput: {
+    width: 343,
+    height: 50,
+    backgroundColor: '#E8E8E8',
+    color: '#BDBDBD',
+    fontFamily: 'Inter_500Medium',
+    fontSize: 16,
+    top: 10,
+  },
+  trackButtonContainer: {
+    width: 343,
+    height: 50,
+    backgroundColor: '#A500FF',
+    borderRadius: 100,
+    top: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  trackButtonText: {
+    color: '#fff',
+    fontFamily: 'Inter_600SemiBold',
+    fontSize: 16,
+  },
 })
 
 export default Home;
