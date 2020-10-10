0a4cc655430f9080d2e285ff169adef3dc3b41f7
arthrc <arthurcastilhos@gmail.com>
2020-10-10 12:22:09 -0300

    formated

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,15 +1,59 @@
 import { rastro } from 'rastrojs';
-import React, { useState, useEffect, useCallback } from 'react';
+import React from 'react';
 import { ScrollView } from 'react-native';
 
 import { Box, BottomBox, City, Date, Description, Time, Title } from './styles';
 
 export default function Feed() {
-  const [feed, setFeed] = useState([]);
 
-  async function loadPage() {
-    const track = await rastro.track(code)
+    const res = await rastro.track(code);
+    const response = res[0];
+    const data = await response.json();
 
-    const data = await track.json();
-  }
-};
+  if (data.status == 'Objeto postado') {
+  return (
+      <ScrollView>
+        <Box>
+          <Title></Title>
+          <BottomBox>
+            <Image source={require('../../assets/posted.png')}/>
+            <Description>
+              <City></City>
+              <Date></Date>
+              <Time></Time>
+            </Description>
+          </BottomBox>
+        </Box>
+      </ScrollView>
+  )} else if (data.status == 'Objeto entregue ao destinatário') {
+    return (
+      <ScrollView>
+        <Box>
+          <Title></Title>
+          <BottomBox>
+            <Image source={require('../../assets/check.png')}/>
+            <Description>
+              <City></City>
+              <Date></Date>
+              <Time></Time>
+            </Description>
+          </BottomBox>
+        </Box>
+      </ScrollView>
+  )} else {
+    return (
+      <ScrollView>
+        <Box>
+          <Title></Title>
+          <BottomBox>
+            <Image source={require('../../assets/shippingpurple.png')}/>
+            <Description>
+              <City></City>
+              <Date></Date>
+              <Time></Time>
+            </Description>
+          </BottomBox>
+        </Box>
+      </ScrollView>
+    )}
+}
