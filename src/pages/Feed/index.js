a60694cae5659c20d3941e8de1be74a89dbfb6d5
arthrc <arthurcastilhos@gmail.com>
2020-10-10 10:32:13 -0300

    start API request

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,3 +1,4 @@
+import { rastro } from 'rastrojs';
 import React, { useState, useEffect, useCallback } from 'react';
 import { ScrollView } from 'react-native';
 
@@ -6,7 +7,9 @@ import { Box, BottomBox, City, Date, Description, Time, Title } from './styles';
 export default function Feed() {
   const [feed, setFeed] = useState([]);
 
-  const response = await fetch(
-    
-  );
+  async function loadPage() {
+    const track = await rastro.track(code)
+
+    const data = await track.json();
+  }
 };
